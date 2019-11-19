94d61e1f4e3737946b160690218df6d9d224019c
Akramurridjal Rahman <xerod@outlook.com>
2019-11-20 00:12:35 +0700

    Initial dist subtree commit

---
+++ dist/precache-manifest.84bbc38a5a9f8ff82277b9f9b0db6948.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4818ead9c9a8dae2ce7",
+    "url": "/<TKMPage>/css/app.bceddef4.css"
+  },
+  {
+    "revision": "1ab7ef9fa5adaa1ace04092ad5f0734a",
+    "url": "/<TKMPage>/css/style.css"
+  },
+  {
+    "revision": "e57493005764c55900507159a1206819",
+    "url": "/<TKMPage>/img/illustration-footer.e5749300.png"
+  },
+  {
+    "revision": "c895dd90f48d5e543b5e5c6091a37fd8",
+    "url": "/<TKMPage>/img/logo-dinkes.c895dd90.png"
+  },
+  {
+    "revision": "9075269cf91047ab6cdae47bd704b722",
+    "url": "/<TKMPage>/img/logo-kotamadya.9075269c.png"
+  },
+  {
+    "revision": "994e272ec00fe657499b2ff45d2bd12a",
+    "url": "/<TKMPage>/img/logo.994e272e.png"
+  },
+  {
+    "revision": "994e272ec00fe657499b2ff45d2bd12a",
+    "url": "/<TKMPage>/img/logo.png"
+  },
+  {
+    "revision": "8fb2b0a1d633446c47fd097666799073",
+    "url": "/<TKMPage>/img/section-1.8fb2b0a1.png"
+  },
+  {
+    "revision": "5ff625822861770428b892d883112650",
+    "url": "/<TKMPage>/index.html"
+  },
+  {
+    "revision": "abd3154689c3981fd45e",
+    "url": "/<TKMPage>/js/about.06031a1f.js"
+  },
+  {
+    "revision": "e4818ead9c9a8dae2ce7",
+    "url": "/<TKMPage>/js/app.89ebf1b0.js"
+  },
+  {
+    "revision": "a6aa20a3296d6add2deb",
+    "url": "/<TKMPage>/js/chunk-vendors.f91f4d27.js"
+  },
+  {
+    "revision": "aa0c1f53463e3f698610b24ca34394ee",
+    "url": "/<TKMPage>/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<TKMPage>/robots.txt"
+  }
+]);
