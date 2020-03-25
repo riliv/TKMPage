11a02a0f2068d709febe3d9c55873d0034cf94b8
Akramurridjal Rahman <xerod@outlook.com>
2020-03-25 16:43:45 +0700

    deploy

---
+++ precache-manifest.db91fc2d9f7c2bdf957ddbc316169314.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89a6428b48fa3a7c73f0",
+    "url": "/TKMPage/css/app.3d1df29b.css"
+  },
+  {
+    "revision": "4f7d99d295ab019a0254",
+    "url": "/TKMPage/css/chunk-vendors.e5ab74a0.css"
+  },
+  {
+    "revision": "694c4e9eb3ed314376e1bf3ca3554d7c",
+    "url": "/TKMPage/css/style.css"
+  },
+  {
+    "revision": "a31c19f58526c80e1bbbb6a1aab7de6f",
+    "url": "/TKMPage/img/bg_biru.a31c19f5.png"
+  },
+  {
+    "revision": "f2d010dfb78e8f063bc6152b128ad6e2",
+    "url": "/TKMPage/img/bg_orange.f2d010df.png"
+  },
+  {
+    "revision": "9da238a51d676e02d70ed9c86f0da29e",
+    "url": "/TKMPage/img/bg_ungu.9da238a5.png"
+  },
+  {
+    "revision": "94232b83c8aa04244944f1e7bc7e9aa3",
+    "url": "/TKMPage/img/illustration-coupon.94232b83.png"
+  },
+  {
+    "revision": "a21d66c33f65beabbeccc36830c2fcbe",
+    "url": "/TKMPage/img/illustration-doctor.a21d66c3.png"
+  },
+  {
+    "revision": "e57493005764c55900507159a1206819",
+    "url": "/TKMPage/img/illustration-footer.e5749300.png"
+  },
+  {
+    "revision": "4c13a28f7f89ad19cdfa8243350adc2b",
+    "url": "/TKMPage/img/illustration-friends.4c13a28f.png"
+  },
+  {
+    "revision": "c895dd90f48d5e543b5e5c6091a37fd8",
+    "url": "/TKMPage/img/logo-dinkes.c895dd90.png"
+  },
+  {
+    "revision": "9075269cf91047ab6cdae47bd704b722",
+    "url": "/TKMPage/img/logo-kotamadya.9075269c.png"
+  },
+  {
+    "revision": "994e272ec00fe657499b2ff45d2bd12a",
+    "url": "/TKMPage/img/logo.994e272e.png"
+  },
+  {
+    "revision": "994e272ec00fe657499b2ff45d2bd12a",
+    "url": "/TKMPage/img/logo.png"
+  },
+  {
+    "revision": "8fb2b0a1d633446c47fd097666799073",
+    "url": "/TKMPage/img/section-1.8fb2b0a1.png"
+  },
+  {
+    "revision": "7525adf02144c9dbedaee13281ec92bb",
+    "url": "/TKMPage/img/section-2.7525adf0.png"
+  },
+  {
+    "revision": "3af00fa92ff8e4ceeda39f9e2377518c",
+    "url": "/TKMPage/index.html"
+  },
+  {
+    "revision": "89a6428b48fa3a7c73f0",
+    "url": "/TKMPage/js/app.d2491521.js"
+  },
+  {
+    "revision": "4f7d99d295ab019a0254",
+    "url": "/TKMPage/js/chunk-vendors.b0065623.js"
+  },
+  {
+    "revision": "aa0c1f53463e3f698610b24ca34394ee",
+    "url": "/TKMPage/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TKMPage/robots.txt"
+  }
+]);
